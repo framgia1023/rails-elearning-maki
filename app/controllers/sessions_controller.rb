9620dd96e6287eecf47fe3cbbc719f1450aa4eb1
前田真希 <maki@Makintosh.local>
2018-12-06 17:18:26 +0800

    Fixed indentions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,24 @@
 class SessionsController < ApplicationController
     
-    def new
-    end
+  def new
+  end
 
-    def create
-        user = User.find_by(email: params[:session][:email])
+  def create
+      user = User.find_by(email: params[:session][:email])
 
-        if user && user.authenticate(params[:session][:password])
-            session[:user_id] = user.id
-            flash[:info] = "Welcome back, #{user.name}！"
-            redirect_to root_url
-        else
-            flash[:danger] = "Ooops! Your email or password was incorrect. Please try again!"
-            render 'new'
-        end
-    end
+      if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          flash[:info] = "Welcome back, #{user.name}！"
+          redirect_to root_url
+      else
+          flash[:danger] = "Ooops! Your email or password was incorrect. Please try again!"
+          render 'new'
+      end
+  end
 
-    def destroy
-        session.delete(:user_id)
-        redirect_to login_url
-    end
+  def destroy
+      session.delete(:user_id)
+      redirect_to login_url
+  end
+  
 end
