e54a6139c19805dfacf4306c9a663f8c6128d6e7
前田真希 <maki@Makintosh.local>
2018-12-11 08:12:12 +0800

    Admin category index page (include update/delete function)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,21 +3,13 @@ class RelationshipsController < ApplicationController
     def create
         user = User.find(params[:followed_id])
         current_user.follow(user)
-        if action_name == "index"
-        redirect_to users_path
-        else
-        redirect_to user
-        end
+        redirect_to request.referer
     end
 
     def destroy
         user = Relationship.find(params[:id]).followed
         current_user.unfollow(user)
-        if action_name == "index"
-         redirect_to users_path
-        else
-        redirect_to user
-        end
+        redirect_to request.referer
     end
-
+    
 end
