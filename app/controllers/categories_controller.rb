8c6441026e754bd3eb2573c5e737574cc0ef9261
前田真希 <maki@Makintosh.local>
2018-12-14 16:18:01 +0800

    Removed category before id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
     def index
-        @categories = Category.joins(:words).group(:category_id).paginate(page: params[:page], per_page: 4)
+        @categories = Category.joins(:words).group(:id).paginate(page: params[:page], per_page: 4)
     end
 end
