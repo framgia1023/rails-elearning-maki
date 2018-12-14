187d7af6943e3ebe589e6bb79041378fdf7363e1
前田真希 <maki@Makintosh.local>
2018-12-14 14:06:06 +0800

    Use_Cat_List

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,5 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 6).order('created_at DESC')
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     has_many :words, dependent: :destroy
-    validates :title, presence: true, length: { maximum: 30 }
+    validates :title, presence: true, length: { maximum: 60 }
 end
