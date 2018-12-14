74649967a299800de846bb06c4f3a0a23468d75b
前田真希 <maki@Makintosh.local>
2018-12-14 16:09:34 +0800

    Added Pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
     def index
-        @categories = Category.paginate(page: params[:page], per_page: 6)
+        @categories = Category.joins(:words).group(:category_id).paginate(page: params[:page], per_page: 4)
     end
 end
