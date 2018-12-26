d3bca2ccc2fbd5f48561db6a066b2455cc50581a
前田真希 <maki@Makintosh.local>
2018-12-26 16:17:57 +0800

    Learned Words 2nd push

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,20 @@
 class CategoriesController < ApplicationController
     def index
-        @learned = current_user.categories.where.not(lessons: {result: nil}).group(:id)
-                    .paginate(page: params[:page], per_page: 6).order('created_at desc')
-
-        @all = Category.joins(:words).group(:id)
-               .paginate(page: params[:page], per_page: 6).order('created_at desc')
-
         @lesson = Lesson.new 
 
         if params[:status] == "learned"
-            @categories = @learned
+            @categories = current_user.learned_words
+                          
         elsif params[:status] == "unlearned"
-            unlearned = (@all - @learned)
-            @categories = Category.where(id: unlearned.map{ |u| u.id })
-                         .paginate(page: params[:page], per_page: 6).order('created_at desc')
+            ids = current_user.learned_words.pluck(:id)
+            @categories = Category.where.not(id: ids)  
         else
-            @categories = @all
+            @categories = Category.all
+                       
         end
            
+        @categories = @categories.joins(:words).group(:id).paginate(page: params[:page], per_page: 6).order('created_at desc')
+
     end
 
     
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,14 +2,15 @@ class WordsController < ApplicationController
 
 def index
     @user = User.find(params[:user_id])
-    @select = @user.categories.where.not(lessons: {result: nil}).pluck(:title)
+    @select = @user.learned_words.pluck(:title) << "All"
 
-    if params[:title].present?
-        @lessons = @user.lessons.joins(:category).where("title = ?", params[:title])
-        @category = Category.find_by(title: params[:title]).title 
-    else 
+
+    if params[:title] == "All" || params[:title].nil?
         @lessons = @user.lessons
         @category = "All"
+    else
+        @lessons = @user.lessons.joins(:category).where("title = ?", params[:title])
+        @category = Category.find_by(title: params[:title]).title 
     end
     
 end
